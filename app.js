605c6595e2121c946ce8cf771d8da62d8e0f6275
Jean <giraldojeanpool@hotmail.com>
2022-11-02 21:10:42 -0500

    added start command

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-//import express
+// Import express
 const express = require("express");
-const swaggerJSDoc = require("swagger-jsdoc");
+// Import middlewares HTTP
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+// Controllers
 const { globalErrorHandler } = require("./controllers/globalError");
+// Routes
 const { mealsRouter } = require("./routes/meal.router");
 const { ordersRouter } = require("./routes/order.router");
 const { restaurantRouter } = require("./routes/restaurant.router");
 const { userRouter } = require("./routes/user.router");
+// Swagger
 const { swaggerUI, swaggerSpec } = require("./swaggerConfig");
+const swaggerJSDoc = require("swagger-jsdoc");
+
 // ejecute express in a var called app
 const app = express();
 // configure to work archives json
 app.use(express.json());
+
+// Added security headers
+
+app.use(helmet());
+
+// Compress responses
+
+app.use(compression());
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+else if (process.env.NODE_ENV === "production") app.use(morgan("combined"));
+
 // define routes here
 
 app.use("/api/v1/users", userRouter);
@@ -18,7 +38,7 @@ app.use("/api/v1/restaurants", restaurantRouter);
 app.use("/api/v1/meals", mealsRouter);
 app.use("/api/v1/orders", ordersRouter);
 app.use(
-  "/api/v1/documentation",
+  "/",
   swaggerUI.serve,
   swaggerUI.setup(swaggerJSDoc(swaggerSpec))
 );
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const startSever = async () => {
     await db.sync();
 
     // set server to listen
-    const PORT = 4000;
+    const PORT = process.env.PORT || 4000;
 
     app.listen(PORT, () => {
       console.log("Express App Running!");
